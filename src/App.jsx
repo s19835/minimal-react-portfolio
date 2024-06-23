c417c92b6630f308f93253c32488c6a9c41e0e77
s19835 <s19835@sci.pdn.ac.lk>
2024-06-23 15:55:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Intro from './components/intro';
+import Portfolio from './components/portfolio';
+
+function App() {
+  return (
+   <div className='App'>
+    <Intro></Intro>
+    <Portfolio></Portfolio>
+   </div>
+  )
+}
+
+export default App
