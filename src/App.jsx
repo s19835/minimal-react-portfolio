eed1dd9b26b806e9891ba225ed6304e702fcab1a
s19835 <s19835@sci.pdn.ac.lk>
2024-06-23 18:04:15 +0530

    Font Added and Intro

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Portfolio from './components/portfolio';
 function App() {
   return (
    <div className='App'>
-    <Intro></Intro>
-    <Portfolio></Portfolio>
+    <Intro />
+    <Portfolio />
    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{jsx,js}"],
   darkMode : 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'josefin-sans': ['"Josefin Sans"', 'sans-serif'],
+      },
+      fontWeight: {
+        100: '100',
+        200: '200',
+        300: '300',
+        400: '400',
+        500: '500',
+        600: '600',
+        700: '700',
+      },
+    },
   },
   plugins: [],
 }
